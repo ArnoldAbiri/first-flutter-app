fedfa454738dee0e12ac0f72fb3b8de5603f635a
Arnold <arnold@mugonat.com>
2025-06-11 14:37:54 +0200

    🎨 Refactor(main.dart): Improves UI with gradient background in the main app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+      body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 3, 7, 250),
+                  Color.fromARGB(108, 35, 53, 117),
+                ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World!'),
+          )
       ),
     ),
   ),
