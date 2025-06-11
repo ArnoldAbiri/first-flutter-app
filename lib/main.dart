76d7953a3e5b85017b54ee70144604e20f7147a3
Arnold <arnold@mugonat.com>
2025-06-11 15:22:39 +0200

    ✨ feat(main.dart): Refactors UI to use a custom GradientContainer widget and styles the text.

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: Scaffold(
-      body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 3, 7, 250),
-                  Color.fromARGB(108, 35, 53, 117),
-                ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text('Hello World!'),
-          )
-      ),
-    ),
+      body: GradientContainer(),
+    )
   ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 3, 7, 250),
+              Color.fromARGB(108, 35, 53, 117),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: const Center(
+          child: Text('Hello World!',
+            style: TextStyle(
+              fontSize: 28,
+              color: Colors.white,
+            ),),
+        )
+    );
+  }
 }
