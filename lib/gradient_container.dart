69e862cfa73c85f524a90675f75c543f4aa52301
Arnold <arnold@mugonat.com>
2025-06-19 15:31:58 +0200

    ✨ feat(gradient_container.dart): Add dice rolling functionality with image and button.

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -4,6 +4,10 @@ import 'package:first_app/my_styled_text.dart';
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
+  void rollDice() {
+    // ....
+  }
+
   @override
   Widget build(context) {
     return Container(
@@ -17,7 +21,18 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: const Center(child: MyStyledText('Hello World!'),
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset(
+                'assets/images/dice-2.png', width: 200,
+            ),
+            TextButton(
+                onPressed: rollDice,
+                child: const MyStyledText('Roll Dice'),
+            )
+          ],
+        ),
       ),
     );
   }
